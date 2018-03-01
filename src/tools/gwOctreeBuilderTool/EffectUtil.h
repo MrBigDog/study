d8879b1fe0b8a2b67f96026500ffbe4632e8fcd4
MrBigDog <635669462@qq.com>
2018-03-01 09:41:13 +0800

    tt

--- src/tools/gwOctreeBuilderTool/EffectUtil.h
+++ src/tools/gwOctreeBuilderTool/EffectUtil.h
@@ -8,21 +8,68 @@
 #include <osg/BlendColor>
 #include <osgAnimation/EaseMotion>
 
-//效率高，效果差(因为shader中没加光照);
-void setBloomEffect1(osg::Node* node);
+namespace gwUtil
+{
+	class Effect :public osg::Referenced
+	{
+	public:
+		void install(osg::Node* node) {}
+		void unstall(osg::Node* node) {}
+	};
 
-//修改Color Array的方式，效率低, 效果好;
-void setBloomEffect2(osg::Node* node, const osg::Vec4& color1, const osg::Vec4& color2);
+	class BloomEffect :public Effect
+	{
+	public:
+		BloomEffect() {}
 
-//修改Material的方式，效率比2高，比1稍低，效果同2;
-void setBloomEffect3(osg::Node* node, const osg::Vec4& color1, const osg::Vec4& color2);
+		void install(osg::Node* node) {}
+		void unstall(osg::Node* node) {}
 
-//设置透明;
-void setTransparentEffect(osg::Node* state, float alf);
+		void setBloomColor(const osg::Vec4& color1, const osg::Vec4& color2)
+		{
+			_color1 = color1;
+			_color2 = color2;
+		}
 
-void renderNodeAsPoint(osg::Node* node);
-void renderNodeAsLine(osg::Node* node);
+	private:
+		osg::Vec4 _color1, _color2;
+	};
 
-void addOutlineEffect(osg::Node* node, const osg::Vec4& color, float width);
+	class TransparentEffect :public Effect
+	{
+	public:
+		TransparentEffect() {}
+
+		void install(osg::Node* node) {}
+		void unstall(osg::Node* node) {}
+
+		void setAlfa(float alfa) { _alfa = alfa; }
+
+	private:
+		float _alfa;
+	};
+
+
+
+
+
+
+	//效率高，效果差(因为shader中没加光照);
+	void setBloomEffect1(osg::Node* node);
+
+	//修改Color Array的方式，效率低, 效果好;
+	void setBloomEffect2(osg::Node* node, const osg::Vec4& color1, const osg::Vec4& color2);
+
+	//修改Material的方式，效率比2高，比1稍低，效果同2;
+	void setBloomEffect3(osg::Node* node, const osg::Vec4& color1, const osg::Vec4& color2);
+
+	//设置透明;
+	void setTransparentEffect(osg::Node* state, float alf);
+
+	void renderNodeAsPoint(osg::Node* node);
+	void renderNodeAsLine(osg::Node* node);
+
+	void addOutlineEffect(osg::Node* node, const osg::Vec4& color, float width);
+}
 
 #endif // StateUtil_h__
